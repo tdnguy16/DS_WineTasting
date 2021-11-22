b02d354155506771a4f659e6e2fbfbcb366ebe2a
tdnguy16 <tdnguy16@gmail.com>
2021-11-22 23:48:53 +0700

    Initial commit

---
+++ wine_tasting.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.pipeline import make_pipeline
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import mean_squared_error, r2_score
+import joblib as jl
+
+
+
+
